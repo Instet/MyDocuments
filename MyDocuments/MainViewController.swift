16072e8262ff677118cd4f706442894a54ff8e26
Ruslan <97151324+Instet@users.noreply.github.com>
2022-07-05 19:36:21 +0400

    create project

--- MyDocuments/MainViewController.swift
+++ MyDocuments/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  MyDocuments
 //
 //  Created by Руслан Магомедов on 05.07.2022.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
 
