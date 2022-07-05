4b04654cfb5d269003bdb04f0d170bcf9fa8a728
Ruslan <97151324+Instet@users.noreply.github.com>
2022-07-05 19:11:51 +0400

    Initial Commit

---
+++ MyDocuments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDocuments
+//
+//  Created by Руслан Магомедов on 05.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
